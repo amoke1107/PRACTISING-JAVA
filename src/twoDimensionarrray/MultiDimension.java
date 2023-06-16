fce5682f035cc4022b14612b7609875a7e162230
Moronkola Adijat <horythorkeh@gmail.com>
2023-06-16 18:12:01 +0100

    multidimension array

--- src/twoDimensionarrray/MultiDimension.java
+++ src/twoDimensionarrray/MultiDimension.java
@@ -1,6 +1,6 @@
 package twoDimensionarrray;
 
-public class Dimension {
+public class MultiDimension {
     public static void main(String[] args) {
         int [][] myNumber = {{1, 2}, {3, 4, 5}};
             for (int i = 0; i < myNumber.length; ++i) {
