4f388651b2df53ade530fca3a88f20514d1e39d7
Moronkola Adijat <horythorkeh@gmail.com>
2023-06-16 17:34:10 +0100

    foodlist

--- src/breakcontinue/FoodList.java
+++ src/breakcontinue/FoodList.java
@@ -3,7 +3,7 @@ package breakcontinue;
 public class FoodList {
     public static void main(String[] args) {
         //break and continue
-        String[] list = {"beans", "rice", "yam", "Indomie", "egg"};
+        String[] list = {"beans", "rice", "yam", "Indomie", "egg","potato"};
         for (String food : list) {
             if (food.equals("yam")) {
                 break;
